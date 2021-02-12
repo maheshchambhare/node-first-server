9f7458c62a8573334a5faba5624bc09785b70980
mahesh <chambharem7@gmail.com>
2021-02-12 12:25:32 +0530

    changed

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const port = 3000;
+const fs = require("fs");
 
 const server = http.createServer(function (req, res) {
-  res.write("hello First Node Server");
-  res.end();
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("page not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(port, function (error) {
